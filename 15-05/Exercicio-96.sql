3f679b6e8e6a7f1042790521e2757e5cfad1c9f7
Robson Ribeiro Lucio <44735328+RobsonR07@users.noreply.github.com>
2019-05-15 09:50:50 -0300

    Update Exercicio-96.sql

--- 15-05/Exercicio-96.sql
+++ 15-05/Exercicio-96.sql
@@ -4,21 +4,21 @@ USE ex96;
 
 CREATE TABLE Empregado(
 	Matricula INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(70),
-    Endereco VARCHAR(90),
-    Salario DECIMAL(10,2),
-    Sexo VARCHAR(1),
-    Dt_Nascimento DATE
+    	Nome VARCHAR(70),
+	Endereco VARCHAR(90),
+	Salario DECIMAL(10,2),
+	Sexo VARCHAR(1),
+	Dt_Nascimento DATE
 );
 
 CREATE TABLE Dependente(
 	idDependente INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(70),
-    Grau_Parentesco VARCHAR(2),
-    Sexo VARCHAR(1),
-    Dt_Dascimento DATE,
-    idEmpregado INT,
-    FOREIGN KEY (idEmpregado) REFERENCES Empregado(Matricula)
+	Nome VARCHAR(70),
+	Grau_Parentesco VARCHAR(2),
+	Sexo VARCHAR(1),
+	Dt_Dascimento DATE,
+	idEmpregado INT,
+	FOREIGN KEY (idEmpregado) REFERENCES Empregado(Matricula)
 );
 
 CREATE TABLE Beneficio_Social(
@@ -27,39 +27,38 @@ CREATE TABLE Beneficio_Social(
 
 CREATE TABLE Beneficicio_Depedente(
 	idBeneficio INT,
-    FOREIGN KEY (idBeneficio) REFERENCES Beneficio_Social(idBeneficio),
-    idDependente INT,
-    FOREIGN KEY (idDependente) REFERENCES Dependente(idDependente)
+	FOREIGN KEY (idBeneficio) REFERENCES Beneficio_Social(idBeneficio),
+	idDependente INT,
+	FOREIGN KEY (idDependente) REFERENCES Dependente(idDependente)
 );
 
 CREATE TABLE Projeto(
 	Codigo INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(70)
+	Nome VARCHAR(70)
 );
 
 CREATE TABLE Projeto_Empregado(
 	idEmpregado INT,
-    FOREIGN KEY (idEmpregado) REFERENCES Empregado(Matricula),
-    idProjeto INT,
-    FOREIGN KEY (idProjeto) REFERENCES Projeto(Codigo)
+	FOREIGN KEY (idEmpregado) REFERENCES Empregado(Matricula),
+	idProjeto INT,
+	FOREIGN KEY (idProjeto) REFERENCES Projeto(Codigo)
 );
 
 CREATE TABLE Departamento(
 	Codigo INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(70)
+	Nome VARCHAR(70)
 );
 
 CREATE TABLE Cidade(
 	idCidade INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(70)
+	Nome VARCHAR(70)
 );
 
 CREATE TABLE Projeto_Alocado(
 	idProjeto INT,
-    FOREIGN KEY (idProjeto) REFERENCES Projeto(Codigo),
-    idDepartamento INT,
-    FOREIGN KEY (idDepartamento) REFERENCES Departamento(Codigo),
-    idCidade INT,
-    FOREIGN KEY (idCidade) REFERENCES Cidade(idCidade)
+	FOREIGN KEY (idProjeto) REFERENCES Projeto(Codigo),
+	idDepartamento INT,
+	FOREIGN KEY (idDepartamento) REFERENCES Departamento(Codigo),
+	idCidade INT,
+	FOREIGN KEY (idCidade) REFERENCES Cidade(idCidade)
 );
-
